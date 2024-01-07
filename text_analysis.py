2c487a0586ef2f273ba1ee61eac56fa89e0d18ea
pd <1508732371@qq.com>
2024-01-07 23:09:40 +0800

    update text analysis

---
+++ text_analysis.py
@@ -0,0 +1,30 @@
+import os
+
+list_word_countries = ['朝鲜', '美国', '台湾', '巴西', '以色列', '乌克兰', '意大利']
+
+
+def word_count(files_folder, list_words):
+    result = {}
+    list_files = [os.path.join(files_folder, file) for file in os.listdir(files_folder)]
+    num_lines = 0
+
+    # Iter through files
+    for file_path in list_files:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            f_lines = f.readlines()
+            # Iter through lines
+            for line in f_lines:
+                num_lines += 1
+                # Iter through each word
+                for word in list_words:
+                    if word in line:
+                        count_old = result.get(word, 0)
+                        result[word] = count_old + 1
+
+    print(f"Through {len(list_files)} files, {num_lines} lines | Result: {result}")
+
+    return result
+
+
+if __name__ == '__main__':
+    word_count(files_folder=r"data\news", list_words=list_word_countries)
