8ec5741eaa16bffb6d0ca3bfa0a75d23f28c894b
pd <1508732371@qq.com>
2024-01-07 23:19:37 +0800

    add combine function

--- text_analysis.py
+++ text_analysis.py
@@ -24,6 +24,41 @@ def word_count(files_folder, list_words):
     return result
 
 
+def word_combine(files_folder, list_combine):
+    result = []
+    list_files = [os.path.join(files_folder, file) for file in os.listdir(files_folder)]
+    num_lines = 0
+
+    # Iter through files
+    for file_path in list_files:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            f_lines = f.readlines()
+            # Iter through lines
+            for line in f_lines:
+                num_lines += 1
+                all_in = True
+                # Iter through each word
+                for word in list_combine:
+                    if word not in line:
+                        all_in = False
+                        break
+                # Check all in
+                if not all_in:
+                    continue
+                else:
+                    result.append(line)
+
+    print(f"Finished -> Through {len(list_files)} files, {num_lines} lines")
+
+    return result
+
 if __name__ == '__main__':
     list_word_countries = ['朝鲜', '美国', '台湾', '巴西', '以色列', '乌克兰', '意大利']
-    word_count(files_folder=r"data\news", list_words=list_word_countries)
+    result_count = word_count(files_folder=r"data\news", list_words=list_word_countries)
+    print(result_count)
+
+    list_word_combine = ['美国', '元首']
+    result_combine = word_combine(files_folder=r"data\news", list_combine=list_word_combine)
+    print(f"combine count: {len(result_combine)}")
+    if result_combine:
+        print(result_combine[0])
