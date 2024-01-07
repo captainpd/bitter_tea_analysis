8204e689e30ac98919729fcfb0c59e4ebc8e1c73
pd <1508732371@qq.com>
2024-01-07 23:26:48 +0800

    add line info

--- text_analysis.py
+++ text_analysis.py
@@ -26,11 +26,12 @@ def word_count(files_folder, list_words):
 
 def word_combine(files_folder, list_combine):
     result = []
-    list_files = [os.path.join(files_folder, file) for file in os.listdir(files_folder)]
+    list_files = os.listdir(files_folder)
     num_lines = 0
 
     # Iter through files
-    for file_path in list_files:
+    for file_name in list_files:
+        file_path = os.path.join(files_folder, file_name)
         with open(file_path, 'r', encoding='utf-8') as f:
             f_lines = f.readlines()
             # Iter through lines
@@ -46,11 +47,13 @@ def word_combine(files_folder, list_combine):
                 if not all_in:
                     continue
                 else:
-                    result.append(line)
+                    file_line = f"{file_name} -> {line}"
+                    result.append(file_line)
 
     print(f"Finished -> Through {len(list_files)} files, {num_lines} lines")
 
     return result
+
 
 if __name__ == '__main__':
     list_word_countries = ['朝鲜', '美国', '台湾', '巴西', '以色列', '乌克兰', '意大利']
