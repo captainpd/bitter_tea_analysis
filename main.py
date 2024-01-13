74be31f59ad075f81220bf80f314243422af15a1
pd <1508732371@qq.com>
2024-01-13 15:47:17 +0800

    fix key error

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from text_analysis import word_count, word_combine
 
 
 if __name__ == '__main__':
-    fetch_news(save_folder=r"data/news")
+    # fetch_news(save_folder=r"data/news")
 
     list_word_countries = ['朝鲜', '美国', '台湾', '巴西', '以色列', '乌克兰', '意大利']
     result_count = word_count(files_folder=r"data\news", list_words=list_word_countries)
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     result_combine = word_combine(files_folder=r"data\news", list_combine=list_word_combine)
     print(f"combine count: {len(result_combine)}")
     if result_combine:
-        print(result_combine[0])
+        print(result_combine)
